c12e6c904220dfeee92803ebb2f8f7cdd41555f5
酷甚么喵 <btguigushi@qq.com>
2023-08-12 06:56:59 +0800

    feat: 添加附件元素

---
+++ src/editor-module/sentence-item/index.ts
@@ -0,0 +1,8 @@
+// import { createVNode, render } from 'vue'
+// import { ElButton } from 'element-plus'
+
+// const node = document.createElement('div')
+// const vnode = createVNode(ElButton, { text: 'xxxxx' })
+// render(vnode, node)
+// console.log(node)
+// console.log(vnode)
