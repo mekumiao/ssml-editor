5df66f50b99f74fb5b5cafda95239504b2b37c7b
酷甚么喵 <btguigushi@qq.com>
2023-08-21 16:41:14 +0800

    fix: 错误提示消息分组

--- example/config.ts
+++ example/config.ts
@@ -2,7 +2,7 @@ import { speaker, english } from './api'
 import { ElMessage } from 'element-plus'
 
 export default {
-  handleError: (error) => ElMessage.warning(error),
+  handleError: (error) => ElMessage.warning({ message: error, grouping: true }),
   fetchSpeaker: speaker,
   fetchEnglish: english
 } as SSMLEditorConfig
