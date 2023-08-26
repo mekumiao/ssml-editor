4ca5fa2e3354ab0b448cd2e1279eec61714fc31b
酷甚么喵 <btguigushi@qq.com>
2023-08-26 23:27:52 +0800

    feat: 处理用户复制和粘贴行为

--- src/core/plugin.ts
+++ src/core/plugin.ts
@@ -1,7 +1,16 @@
 import { type IDomEditor } from '@wangeditor/editor'
 
 export default <T extends IDomEditor>(editor: T) => {
-  const { deleteBackward, deleteForward, insertBreak, apply, normalizeNode, insertText } = editor
+  const {
+    deleteBackward,
+    deleteForward,
+    insertBreak,
+    apply,
+    normalizeNode,
+    insertText,
+    insertData,
+    setFragmentData
+  } = editor
   const newEditor = editor
 
   newEditor.deleteBackward = (unit) => {
@@ -17,13 +26,35 @@ export default <T extends IDomEditor>(editor: T) => {
   }
 
   newEditor.normalizeNode = (entry) => {
-    // console.log('normalizeNode', JSON.stringify(entry))
+    console.log('normalizeNode')
     normalizeNode(entry)
   }
 
   newEditor.apply = (operation) => {
     console.log('apply', JSON.stringify(operation))
     apply(operation)
+  }
+
+  /**
+   * 粘贴时如何处理剪贴板文本到编辑器
+   * 1. 忽略编辑器之身的粘贴
+   * 2. 其他粘贴行为一律使用文本粘贴
+   */
+  newEditor.insertData = (data) => {
+    if (data.types.includes('application/x-slate-fragment')) {
+      return insertData(data)
+    } else {
+      insertText(data.getData('text/plain').trim())
+    }
+  }
+
+  /**
+   * 设置复制时设置文字片段格式
+   */
+  newEditor.setFragmentData = (data) => {
+    const plain = data.getData('text/plain').replaceAll(/[\s]/gi, '')
+    data.setData('text/plain', plain)
+    setFragmentData(data)
   }
 
   newEditor.insertText = (text) => {
