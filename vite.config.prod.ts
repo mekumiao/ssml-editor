0eae069e7d7e32b3cbb8df7d26e1b8c3e736fddf
酷甚么喵 <btguigushi@qq.com>
2023-09-05 04:49:13 +0800

    fix: 移除 rollup-plugin-typescript2 插件
    1. 它与dts冲突,导致生成带绝对路径的d.ts文件

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 import { resolve } from 'path'
 import ElementPlus from 'unplugin-element-plus/vite'
-import typescript2 from 'rollup-plugin-typescript2'
 import dts from 'vite-plugin-dts'
 
 export default function (): UserConfig {
@@ -22,26 +21,6 @@ export default function (): UserConfig {
           'vite.config.prod.ts',
         ],
         insertTypesEntry: true,
-      }),
-      typescript2({
-        check: false,
-        include: ['src/**/*.vue'],
-        tsconfigOverride: {
-          compilerOptions: {
-            outDir: 'dist',
-            sourceMap: true,
-            declaration: true,
-            declarationMap: true,
-          },
-        },
-        exclude: [
-          'example/**',
-          'node_modules/**',
-          'src/menu/**/*.tsx',
-          'vite.config.ts',
-          'vite.config.dev.ts',
-          'vite.config.prod.ts',
-        ],
       }),
     ],
     build: {
