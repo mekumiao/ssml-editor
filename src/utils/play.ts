6ab56abd066eab757e795c8222d207f177de4ec6
酷甚么喵 <btguigushi@qq.com>
2023-09-15 11:52:24 +0800

    feat: 给audio标签添加暂停按钮

--- src/utils/play.ts
+++ src/utils/play.ts
@@ -1,41 +1,20 @@
 export class AudioPlayer {
-  private audio: HTMLAudioElement | null
-  private src: string | null
+  private audio: HTMLAudioElement
 
   constructor() {
-    this.audio = null
-    this.src = null
-  }
-
-  private removeAudioElement() {
-    if (this.audio) {
-      document.body.removeChild(this.audio)
-      this.audio = null
-      this.src = null
-    }
+    this.audio = new Audio()
+    this.audio.autoplay = false
+    this.audio.volume = 0.5
   }
 
   play(src: string) {
-    this.stop()
-    this.audio = document.createElement('audio')
-    this.audio.hidden = true
-    this.audio.volume = 0.5
+    this.audio.pause()
     this.audio.src = src
-    this.src = src
-    document.body.appendChild(this.audio)
     this.audio.play()
   }
 
-  stop(src?: string) {
-    if (src && src !== this.src) return
-    if (this.audio) {
-      this.audio.pause()
-      this.audio.currentTime = 0
-      this.removeAudioElement()
-    }
+  stop() {
+    this.audio.pause()
+    this.audio.currentTime = 0
   }
 }
-
-const audioPlayer = new AudioPlayer()
-
-export { audioPlayer }
