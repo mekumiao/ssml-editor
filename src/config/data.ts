a7b36019bb74897308f956e927d424dceb14d7e9
mekumiao <btguigushi@qq.com>
2023-10-23 06:15:00 +0800

    fix: 更换头像url

--- src/config/data.ts
+++ src/config/data.ts
@@ -1,2 +1,1 @@
-export const demoAvatar = () =>
-  'https://gcore.jsdelivr.net/gh/mekumiao/img-hosting/upload/4314c841777e4d20901cd5d04a28e91a.png'
+export const demoAvatar = () => 'https://i.yuxiaoyu.top/upload/4314c841777e4d20901cd5d04a28e91a.png'
