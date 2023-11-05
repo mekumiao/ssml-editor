7ea6ea83685d186edbcbbfe191e724b19d7eee97
mekumiao <btguigushi@qq.com>
2023-11-06 02:51:27 +0800

    fix: 修复试点击听面板文字按钮边框时抖动bug

--- src/menu/try-play/simple-tag.vue
+++ src/menu/try-play/simple-tag.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 defineEmits<{ click: [value: string] }>()
-withDefaults(defineProps<{ activate?: boolean; value?: string }>(), { activate: false, value: '' })
+const props = withDefaults(
+  defineProps<{
+    activate?: boolean
+    value?: string
+    small?: boolean
+  }>(),
+  {
+    activate: false,
+    value: '',
+    small: false,
+  },
+)
+
+const style = computed(() => {
+  return props.small
+    ? {
+        'max-width': '160px',
+      }
+    : {
+        height: '20px',
+        'line-height': '20px',
+        'font-size': '0.65rem',
+        'min-width': '60px',
+        'max-width': '160px',
+      }
+})
 </script>
 
 <template>
   <span
-    class="tag-item d-inline-block text-white text-center text-nowrap text-truncate"
-    :class="{ 'border border-white rounded-pill': activate }"
+    class="tag-item d-inline-block text-white text-center text-nowrap text-truncate rounded-pill"
+    :class="{ 'border border-white': activate, 'px-1': small }"
     @click="$emit('click', value)"
-    style="height: 20px; min-width: 60px; max-width: 160px; font-size: 0.65rem; cursor: pointer"
+    style="cursor: pointer; border: 1px solid transparent; box-sizing: content-box"
+    :style="style"
   >
     <slot></slot>
   </span>
