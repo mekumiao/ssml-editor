fa7bae686c10b56356c87cd3a749e1780bef59bd
mekumiao <btguigushi@qq.com>
2023-11-24 16:57:28 +0800

    refactor: 优化样式

--- src/components/bar-wrapper/bar-wrapper-menus.vue
+++ src/components/bar-wrapper/bar-wrapper-menus.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bar-wrapper-menus">
-    <div class="menus">
+  <div class="bar-wrapper-menus d-flex flex-row justify-content-center align-items-center">
+    <div class="menus d-flex flex-row justify-content-center align-items-center">
       <slot></slot>
     </div>
-    <div class="divider"></div>
+    <div class="divider" style="height: 50px; width: 3px; margin: 1px 14px"></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.bar-wrapper-menus,
-.menus {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
+$colors: var(--tool-green-border-color), var(--tool-cyan-color), var(--tool-orange-color),
+  var(--tool-purple-color), var(--tool-yellow-color);
+
+@for $i from 0 to 5 {
+  .bar-wrapper-menus:nth-child(#{$i + 1}) .divider {
+    background-color: nth($colors, $i + 1);
+  }
 }
 </style>
--- src/components/bar-wrapper/bar-wrapper.vue
+++ src/components/bar-wrapper/bar-wrapper.vue
@@ -1,33 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bar-wrapper">
+  <div class="bar-wrapper d-flex flex-row justify-content-center align-items-center">
     <slot></slot>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.bar-wrapper {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-
-  :deep() {
-    .divider {
-      height: 50px;
-      width: 3px;
-      margin: 1px 14px;
-    }
-
-    $colors: var(--tool-green-border-color), var(--tool-cyan-color), var(--tool-orange-color),
-      var(--tool-purple-color), var(--tool-yellow-color);
-
-    @for $i from 0 to 5 {
-      .bar-wrapper-menus:nth-child(#{$i + 1}) .divider {
-        background-color: nth($colors, $i + 1);
-      }
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
